10fcc49398f3587198b5ef84872713b10717210b
khangnm13 <khangpro2536@gmail.com>
2023-03-17 23:20:36 +0700

    #1 - implement-button-component

---
+++ doannhom/Min-Shop-17-implement-shopping-cart-component/src/components/button/buttonContent/buTtonsign.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './buTton.css'
+function buTtonsign() {
+  return (
+    <div>
+      <button> <a href="#">Sign in</a></button>
+    </div>
+  )
+}
+
+export default buTtonsign
