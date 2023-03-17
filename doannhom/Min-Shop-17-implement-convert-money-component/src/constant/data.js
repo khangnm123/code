d64c812ec8b0ea30af506a4b57fd5f12a6c94c2d
khangnm13 <khangpro2536@gmail.com>
2023-03-18 06:30:40 +0700

    15-implement-convert-money-helper

---
+++ doannhom/Min-Shop-17-implement-convert-money-component/src/constant/data.js
@@ -0,0 +1,83 @@
+export const MoneyVert = [
+    {
+      nameMoney:"Đô La",
+      numberMoney:23590.00000  ,
+    },
+    {
+      nameMoney:"Yên",
+      numberMoney:177.84300,
+    },
+    {
+      nameMoney:"Euro",
+      numberMoney: 25076.20000,
+    },
+    {
+      nameMoney:"Rupee",
+      numberMoney:285.58500,
+    },
+    {
+        nameMoney:"CAD",
+        numberMoney:17203.90000,
+      },
+      {
+        nameMoney:"AUD",
+        numberMoney:15792.30000,
+      },
+      {
+        nameMoney:"CHF",
+        numberMoney:25425.70000 ,
+      },
+      {
+        nameMoney:"MXN",
+        numberMoney: 1256.95000,
+      },
+      {
+        nameMoney:"FJD",
+        numberMoney:10655.60000,
+      },
+      {
+        nameMoney:"SCR",
+        numberMoney:1785.28000,
+      },
+      {
+        nameMoney:"BBD",
+        numberMoney: 11683.40000,
+      },
+      {
+        nameMoney:"GTQ",
+        numberMoney:3024.36000,
+      },
+      {
+        nameMoney:"CLP",
+        numberMoney:28.58350 ,
+      },
+      {
+        nameMoney:"HNL",
+        numberMoney:956.46300 ,
+      },
+      {
+        nameMoney:"UGX",
+        numberMoney: 6.29486,
+      },
+      {
+        nameMoney:"ZAR",
+        numberMoney: 1284.08000,
+      },
+      {
+        nameMoney:"TND",
+        numberMoney: 7594.98000,
+      },
+      {
+        nameMoney:"BSD",
+        numberMoney:23590.00000,
+      },
+      {
+        nameMoney:"SLL",
+        numberMoney:1.14551 ,
+      },
+      {
+        nameMoney:"GMD",
+        numberMoney:371.20400,
+      },
+
+    ]
