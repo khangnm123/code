2c05f8fd804892c6d564663df90df778762d850c
khangnm13 <khangpro2536@gmail.com>
2023-03-18 22:27:17 +0700

    #1 - implement-button

---
+++ doannhom/Min-Shop-17-implement-shopping-cart/src/App.jsx
@@ -0,0 +1,14 @@
+
+import MainLayout from "./containers/MainLayout";
+import "./assets/styles/index.scss";
+import ButtonContauner from './components/button/ButtonContauner'
+
+function App() {
+  return (
+    <MainLayout>
+      <ButtonContauner/>
+    </MainLayout>
+  );
+}
+
+export default App;
